aa2e148b1a6ab93b790e35cd400b64cfc449d964
java2019 <java_2019@mail.ru>
2017-02-07 10:43:25 +0300

    Step 2

--- app/src/main/java/com/example/use/bitsandpizzasfrag/StoresFragment.java
+++ app/src/main/java/com/example/use/bitsandpizzasfrag/StoresFragment.java
@@ -2,7 +2,7 @@ package com.example.use.bitsandpizzasfrag;
 
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
